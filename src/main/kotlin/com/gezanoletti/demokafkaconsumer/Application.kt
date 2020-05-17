d1db6bc2dfa2b6a1b95a3b36f922b7bccbb1acd9
gezanoletti <gezanoletti@gmail.com>
2020-05-17 18:40:36 +0200

    Consume from a sink topic (Kafka Stream)

--- src/main/kotlin/com/gezanoletti/demokafkaconsumer/Application.kt
+++ src/main/kotlin/com/gezanoletti/demokafkaconsumer/Application.kt
@@ -2,11 +2,14 @@ package com.gezanoletti.demokafkaconsumer
 
 import com.gezanoletti.demokafkaconsumer.KafkaConstants.MAX_POLL_RECORDS
 import com.gezanoletti.demokafkaconsumer.KafkaConstants.OFFSET_RESET_EARLIER
+import com.gezanoletti.demokafkaconsumer.KafkaConstants.SCHEMA_REGISTRY_URL
 import com.gezanoletti.demokafkaconsumer.KafkaConstants.TOPIC_NAME_AVRO
+import com.gezanoletti.demokafkaconsumer.KafkaConstants.TOPIC_NAME_AVRO_STREAM
 import com.gezanoletti.demokafkaconsumer.KafkaConstants.TOPIC_NAME_COMPACT
 import com.gezanoletti.demokafkaconsumer.KafkaConstants.TOPIC_NAME_NORMAL
 import com.gezanoletti.demokafkaproducer.avro.PersonMessageKey
 import com.gezanoletti.demokafkaproducer.avro.PersonMessageValue
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG
 import org.apache.kafka.clients.consumer.ConsumerConfig.*
@@ -38,17 +41,17 @@ fun main() {
             MAX_POLL_RECORDS_CONFIG to MAX_POLL_RECORDS,
             ENABLE_AUTO_COMMIT_CONFIG to "false",
             AUTO_OFFSET_RESET_CONFIG to OFFSET_RESET_EARLIER,
-            "schema.registry.url" to "http://localhost:8081",
+            SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
             SPECIFIC_AVRO_READER_CONFIG to true
         )
     )
-    consumer2.subscribe(listOf(TOPIC_NAME_AVRO))
+    consumer2.subscribe(listOf(TOPIC_NAME_AVRO, TOPIC_NAME_AVRO_STREAM))
 
     try {
         while (true) {
             val records1 = consumer1.poll(Duration.ofMillis(10))
             records1.forEach {
-                println("Record: key=${it.key()}, value=${it.value()}, partition=${it.partition()}, offset=${it.offset()}")
+                println("Record: key=${it.key()}, value=${it.value()}, topic=${it.topic()}, partition=${it.partition()}, offset=${it.offset()}")
             }
             consumer1.commitAsync()
 
@@ -56,7 +59,7 @@ fun main() {
             records2.forEach {
                 println(
                     "Record: key=${it.key().getId()}, value=[${it.value().getName()} ${it.value()
-                        .getSurname()}], partition=${it.partition()}, offset=${it.offset()}"
+                        .getSurname()}], topic=${it.topic()}, partition=${it.partition()}, offset=${it.offset()}"
                 )
             }
             consumer2.commitAsync()
@@ -78,7 +81,9 @@ object KafkaConstants {
     const val TOPIC_NAME_NORMAL = "demo-basic-kafka-partitions"
     const val TOPIC_NAME_COMPACT = "demo-basic-kafka-partitions-compact"
     const val TOPIC_NAME_AVRO = "demo-basic-kafka-partitions-avro"
+    const val TOPIC_NAME_AVRO_STREAM = "demo-basic-kafka-partitions-avro-stream"
     //    const val OFFSET_RESET_LATEST = "latest"
     const val OFFSET_RESET_EARLIER = "earliest"
     const val MAX_POLL_RECORDS = 10
+    const val SCHEMA_REGISTRY_URL = "http://localhost:8081"
 }
